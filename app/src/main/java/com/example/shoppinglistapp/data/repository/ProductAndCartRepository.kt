8555ab93ea7c8250f414ef492b226868335f16be
vinita <vinitavp@ix.lentra.ai>
2023-02-03 18:39:36 +0530

    package changes

--- app/src/main/java/com/example/shoppinglistapp/data/repository/ProductAndCartRepository.kt
+++ app/src/main/java/com/example/shoppinglistapp/data/repository/ProductAndCartRepository.kt
@@ -2,7 +2,9 @@ package com.example.shoppinglistapp.domain.repository
 
 import com.example.shoppinglistapp.data.networkservices.ApiServices
 
-class ProductListRepository {
+class ProductAndCartRepository {
 
     suspend fun getProductList(product_category_id: String,limit: String,page: String) =ApiServices.getInstance().ProductItemList(product_category_id,limit,page)
+
+    suspend fun getProductDetail(product_id:String) = ApiServices.getInstance().ProductDetail(product_id)
 }
